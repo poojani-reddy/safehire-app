bd9be8b2f94cd1abacb26d1035fd7574a4100656
poojani-reddy <167430682+poojani-reddy@users.noreply.github.com>
2025-07-05 15:41:24 +0530

    API KEY

--- demo - model workingCopy/lib/api_key.dart
+++ demo - model workingCopy/lib/api_key.dart
@@ -1,5 +1,2 @@
-const String GROQ_API_KEY = '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';
-
-//'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';
-//const String GROQ_API_KEY = "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";
-const String firebaseApiKey = 'AIzaSyAeE7GZqQKPhy9Smptu_0ojQ2CG3Lrj5pE';
+const String GROQ_API_KEY = 'GROQ API KEY';
+const String firebaseApiKey = 'FIREBASE API KEY';
